14c4404fbf9fa0bfbba9ca2e52358976ec89bd91
Mariana Silva <mariana.silva_dev@hotmail.com>
2026-03-22 20:24:56 +0000

    desenvolvimento de um componente header simples

---
+++ src/presentation/components/Header.tsx
@@ -0,0 +1,10 @@
+import "../styles/Header.css";
+
+export const Header = () => {
+  return (<header className="header">
+        <div className="container">
+            <h1>Dentsu</h1>
+        </div>
+    </header>
+  );
+};
